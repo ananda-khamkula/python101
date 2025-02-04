accce9ff32a88d42b6447b66d0190202a3805c4f
ananda-khamkula <ananda.kha@gmail.com>
2025-02-04 16:12:01 +0700

    Update 1

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello World")
 print("I am Ananda")
+print("I am the third one")
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/ananda-khamkula/python101.git
+#git push -u origin main
